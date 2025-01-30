f16c23cc87367fd03b80fcbb135d58217e002e39
surajjagtap9836 <surajjagtap9836@gmail.com>
2025-01-30 18:21:40 +0530

    add toaster

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,36 +1,36 @@
-import React, { useState } from 'react'
-import plus from "./../../images/plus.svg" 
-import './Todo.css'
+import React, { useState } from 'react';
+import toast,  { Toaster} from 'react-hot-toast'
+import plus from "./../../images/plus.svg" ;
+import './Todo.css';
+
 function Todo() {
-  const  [todolist,settodolist]=useState(["go to shop", "call", "play"]);
+  const  [todolist,settodolist]=useState(["Go to College"]);
  const [newtodo,setnewtodo]=useState([""]);
 
   let additem=()=>{
     settodolist([...todolist,newtodo]);
-    setnewtodo("")
+    setnewtodo("") 
+    toast.success("Task Added successfully !")
   }
-
+ 
  let update=(event)=>{
   setnewtodo(event.target.value);
-
  }
 
   return (
     <>
     <div className='main'>
+      
         <h1 className='todo-title'>Todo List📝</h1>
         <div className='container'>
           <p className='name'>List items...</p>
          {
           todolist.map((todos)=>{
             return(
-
             <h3 className='items'>{todos}</h3>
           )
-
           })
          }
-
 
         </div>
 
@@ -46,12 +46,14 @@ function Todo() {
             <img src={plus} 
             className='add-btn'
             onClick={additem} />
+            
           </>
         </div>
-      
+        <Toaster/>
     </div>
     </>
   )
+  
 }
 
 export default Todo
